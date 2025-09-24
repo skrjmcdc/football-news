8e8863e89cbfff46419862cbe73f763652b47cfb
Muhammad Ibaadi Ilmi <muhammad.ibaadi@ui.ac.id>
2025-09-24 13:42:42 +0700

    Tutorial 4 part 1

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import show_main, create_news, show_news, show_xml, show_json, \
-        show_xml_by_id, show_json_by_id, register, login_user, logout_user
+from main.views import show_main, create_news, show_news, edit_news, delete_news
+from main.views import show_xml, show_json, show_xml_by_id, show_json_by_id
+from main.views import register, login_user, logout_user
 
 app_name = 'main'
 
@@ -8,6 +9,8 @@ urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-news/', create_news, name="create_news"),
     path('news/<str:id>/', show_news, name="show_news"),
+    path('news/<uuid:id>/edit', edit_news, name='edit_news'),
+    path('news/<uuid:id>/delete', delete_news, name='delete_news'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
--- main/views.py
+++ main/views.py
@@ -57,6 +57,24 @@ def show_news(request, id):
 
     return render(request, "news_detail.html", context)
 
+def edit_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    form = NewsForm(request.POST or None, instance=news)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, "edit_news.html", context)
+
+def delete_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.delete()
+    return HttpResponseRedirect(reverse('main:show_main'))
+
 def show_xml(request):
 
     news_list = News.objects.all()
